b50a27c8f0ac27d37e4409eeea5c3e61de9b5f64
Delbert <code@delbert.me>
2015-11-07 23:38:19 +0800

    Session 3.2.2

--- 03/03.js
+++ 03/03.js
@@ -1,5 +1,16 @@
 $(document).ready(function(){
 	$('#switcher-large').click(function(){
+		$('body').removeClass('narrow');
 		$('body').addClass('large');
 	});
+
+    $("#switcher-narrow").click(function(){
+        $("body").removeClass("large");
+        $("body").addClass("narrow");
+    });
+
+    $("#switcher-default").click(function(){
+        $("body").removeClass("large");
+        $("body").removeClass("narrow");
+    });
 });
