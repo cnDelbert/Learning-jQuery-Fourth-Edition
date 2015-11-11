cb89519d64de1cea13b44025e5c7d2ee51e0735f
Delbert <code@delbert.me>
2015-11-11 23:00:34 +0800

    list 3-18 bad example

--- 03/03.js
+++ 03/03.js
@@ -1,14 +1,12 @@
 $(document).ready(function() {
     $('#switcher-default').addClass('selected');
-    $('#switcher button').click(function(event) {
-        if(event.target.is("button")){
-            var bodyClass = this.id.split('-')[1];  // The id and css properties should be the same.
-            $('body').removeClass().addClass(bodyClass);
-            $('#switcher button').removeClass('selected');
-            $(event.target).addClass('selected');
-        } else {
+    $('#switcher').click(function(event) {
+        if (!$(event.target).is('button')) {
             $('#switcher button').toggleClass('hidden');
         }
+    });
+    $("#switcher-narrow, #switcher-large").click(function(){
+        $("#switcher").off("click");
     });
 
     // list 3-10
