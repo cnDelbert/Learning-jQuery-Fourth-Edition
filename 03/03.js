bb1fd18ce94b810efcecc769ff45cb377f0f9b04
Delbert <code@delbert.me>
2015-11-13 00:01:32 +0800

    Chapter 3 Final: Bug still, would not recover the click function after buttons clicked.

--- 03/03.js
+++ 03/03.js
@@ -1,25 +1,55 @@
-$(document).ready(function() {
+$(document).ready(function(){
+    // 在switcher上启用悬停效果(Not only the <h3> tag)
+    $("#switcher").hover(
+        function(){
+            $(this).addClass("hover");
+        },
+        function(){
+            $(this).removeClass("hover");
+        }
+    );
+
+    // 创建一个复用函数
+    var toggleSwitcher = function(event){
+        if(!$(event.target).is("button")){
+            $("#switcher button").toggleClass("hidden");
+        }
+    };
+
+    $("#switcher").on("click", toggleSwitcher);
+
+    // 模拟一次单击事件,以便折叠switcher
+    $("#switcher").click();
+
+    var setBodyClass = function(className){
+        $("body").removeClass().addClass(className);
+        $("#switcher button").removeClass("selected");
+        $("#switcher-" + className).addClass("selected");
+        $("#switcher").off("click", toggleSwitcher);
+        if(className == 'default'){
+            $("switcher").on("click", toggleSwitcher);
+        }
+    };
+
     $('#switcher-default').addClass('selected');
-    $('#switcher').on('click.collapse', function(event) {
-        if (!$(event.target).is('button')) {
-            $('#switcher button').toggleClass('hidden');
+
+    $('#switcher').click(function(event) {
+        if ($(event.target).is('button')) {
+            var bodyClass = event.target.id.split('-')[1];
+            setBodyClass(bodyClass);
         }
     });
-    $('#switcher-narrow, #switcher-large').click(function() {
-        $('#switcher').off('click.collapse');
+
+    var triggers = {
+        D: 'default',
+        N: 'narrow',
+        L: 'large'
+    };
+
+    $(document).keyup(function(event) {
+        var key = String.fromCharCode(event.keyCode);
+        if (key in triggers) {
+            setBodyClass(triggers[key]);
+        }
     });
-
-    // list 3-10
-    //$("#switcher h3").hover(function(){
-    //    $(this).addClass("hover");
-    //},function(){
-    //    $(this).removeClass("hover");
-    //});
-
-    //list 3-11
-    //$("#switcher").click(function(event){
-    //    if(event.target == this){
-    //        $("#switcher button").toggleClass("hidden");
-    //    }
-    //});
 });
