1b56af5ffe3168668a2f2030bae1eacc0c34150f
Delbert <code@delbert.me>
2015-11-24 00:00:48 +0800

    list 4-25 bad sample

--- 04/04.js
+++ 04/04.js
@@ -54,9 +54,9 @@ $(document).ready(function(){
 
     $('p').eq(2).css('border', '1px solid #333')
         .click(function(){
-            $(this).slideUp("slow")
-                .next()
-                .slideDown();
+            $(this).next().slideDown("slow", function(){
+                $(this).slideUp("slow");
+            });
         });
     $('p').eq(3).css('backgroundColor', '#ccc').hide();
 });
