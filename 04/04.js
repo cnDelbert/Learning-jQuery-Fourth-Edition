c5ebc6f36a42791c992e50a4131f29fda625b678
Delbert <code@delbert.me>
2015-11-24 00:02:02 +0800

    list 4-26 animate in a queue

--- 04/04.js
+++ 04/04.js
@@ -54,8 +54,9 @@ $(document).ready(function(){
 
     $('p').eq(2).css('border', '1px solid #333')
         .click(function(){
-            $(this).next().slideDown("slow", function(){
-                $(this).slideUp("slow");
+            var $clickedItem = $(this);
+            $clickedItem.next().slideDown("slow", function(){
+                $clickedItem.slideUp("slow");
             });
         });
     $('p').eq(3).css('backgroundColor', '#ccc').hide();
