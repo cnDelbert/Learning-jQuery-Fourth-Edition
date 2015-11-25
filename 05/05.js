92980481205a4ff2b75a2afd607f58a52f95c922
Delbert <code@delbert.me>
2015-11-25 23:54:36 +0800

    list 5-3 how id increased?

--- 05/05.js
+++ 05/05.js
@@ -8,6 +8,9 @@
 $(document).ready(function(){
     $("div.chapter a").attr({
         rel: "external",
-        title: "Learn more at Wikipedia"
+        title: "Learn more at Wikipedia",
+        id: function(index, oldValue){
+            return "wikilink-" + index;
+        }
     });
 });
