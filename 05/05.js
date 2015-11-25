194f15ef2f66e01ab41adbb06c25363e53e0c04d
Delbert <code@delbert.me>
2015-11-26 00:01:17 +0800

    list 5-4

--- 05/05.js
+++ 05/05.js
@@ -6,7 +6,7 @@
 // See README.txt for more information.
 
 $(document).ready(function(){
-    $("div.chapter a").attr({
+    $("div.chapter a[href^='wikipedia']").attr({
         rel: "external",
         title: "Learn more at Wikipedia",
         id: function(index, oldValue){
