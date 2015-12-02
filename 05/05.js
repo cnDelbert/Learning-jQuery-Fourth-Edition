d97942aefdd17ae40551066bd02571af4c63f0ac
Delbert <code@delbert.me>
2015-12-02 23:50:43 +0800

    remove comment

--- 05/05.js
+++ 05/05.js
@@ -1,10 +1,3 @@
-// This is the custom JavaScript file referenced by index.html. You will notice
-// that this file is currently empty. By adding code to this empty file and
-// then viewing index.html in a browser, you can experiment with the example
-// page or follow along with the examples in the book.
-//
-// See README.txt for more information.
-
 $(document).ready(function(){
     $("div.chapter a[href*='wikipedia']").attr({
         rel: "external",
