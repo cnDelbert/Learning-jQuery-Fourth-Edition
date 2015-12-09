0f94839693bc99ffc1f37970a6696b76154265ff
Delbert <code@delbert.me>
2015-12-09 23:42:00 +0800

    nth changed

--- 05/05.js
+++ 05/05.js
@@ -26,6 +26,7 @@ $(document).ready(function(){
     });
 
     var $clonedCopy = $(this).clone();
-    $clonedCopy.prependTo($parentParagraph);
+    $clonedCopy.addClass('pulled')
+        .prependTo($parentParagraph);
 
 });
