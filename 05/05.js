5d164469785e2b2adf55a908c7e25f954f5c3400
Delbert <code@delbert.me>
2015-12-10 00:09:39 +0800

    emphrasis

--- 05/05.js
+++ 05/05.js
@@ -23,10 +23,10 @@ $(document).ready(function(){
     $('span.pull-quote').each(function(index) {
         var $parentParagraph = $(this).parent('p');
         $parentParagraph.css('position', 'relative');
-    });
 
-    var $clonedCopy = $(this).clone();
-    $clonedCopy.addClass('pulled')
-        .prependTo($parentParagraph);
+        var $clonedCopy = $(this).clone();
+        $clonedCopy.addClass('pulled')
+            .prependTo($parentParagraph);
+    });
 
 });
