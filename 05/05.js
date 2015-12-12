a2e5dc56c9b4c81ad29ea8a48b0d458bbd1517ec
Delbert <code@delbert.me>
2015-12-12 23:36:27 +0800

    back to list 5-8

--- 05/05.js
+++ 05/05.js
@@ -12,25 +12,6 @@ $(document).ready(function(){
     $("<a href='#top'>Back to top</a>").insertAfter("div.chapter p");
 
     $("<a id='top'></a>").prependTo("body");
-
-    var $notes = $('<ol id="notes"></ol>').insertBefore('#footer');
-
-    $('span.footnote').each(function(index) {
-        $(this).before(['<sup>', index + 1, '</sup>'].join(" "))
-            .appendTo($notes).wrap('<li></li>');
-    });
-
-    $('span.pull-quote').each(function(index) {
-        var $parentParagraph = $(this).parent('p');
-        $parentParagraph.css('position', 'relative');
-
-        var $clonedCopy = $(this).clone();
-        $clonedCopy.addClass('pulled')
-            .find('span.drop')
-            .html('&hellip;')
-            .end()
-            .text($clonedCopy.text())
-            .prependTo($parentParagraph);
-    });
+    
 
 });
