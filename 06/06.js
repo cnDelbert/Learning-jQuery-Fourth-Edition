ab1d9a764910bba8df924b96d99572effe1a54d4
Delbert <code@delbert.me>
2015-12-25 22:28:38 +0800

    list 6-2

--- 06/06.js
+++ 06/06.js
@@ -8,5 +8,7 @@ $(document).ready(function() {
     $('#letter-a a').click(function(event) {
         event.preventDefault();
         $("#dictionary").load("a.html");
-    })
+        alert("loaded");
+    });
+
 });
