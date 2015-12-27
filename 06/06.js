321065a359f3d9527d5c6f675e6cbd7e64ca1dbc
Delbert <code@delbert.me>
2015-12-27 23:32:45 +0800

    list 6-5

--- 06/06.js
+++ 06/06.js
@@ -14,7 +14,15 @@ $(document).ready(function() {
     $('#letter-b a').click(function(event){
         event.preventDefault();
         $.getJSON("b.json", function(data){
-            
+            var html = '';
+            $.each(data, function(entryIndex, entry){
+                html += "<div class='entry'>";
+                html += "<h3 class='term'>" + entry.term + "</h3>";
+                html += "<div class='part'>" + entry.part + "</div>";
+                html += "<div class='definition'>" + entry.definition + "</div>";
+                html += "</div>";
+            });
+            $('#dictionary').html(html);
         });
         //$('#dictionary');
     })
