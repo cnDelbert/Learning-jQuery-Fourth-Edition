87188f720c33752cc4b5af46924b34a6628ff932
Delbert <code@delbert.me>
2016-01-01 22:42:42 +0800

    xml

--- 06/06.js
+++ 06/06.js
@@ -15,7 +15,32 @@ $(document).ready(function() {
         event.preventDefault();
         //$.getScript('c.js');
         $.get('d.xml', function(data) {
-            
+            $('#dictionary').empty();
+            $(data).find('entry').each(function() { 4
+                var $entry = $(this);
+                var html = '<div class="entry">';
+                html += '<h3 class="term">' + $entry.attr('term');
+                html += '</h3>';
+                html += '<div class="part">' + $entry.attr('part');
+                html += '</div>';
+                html += '<div class="definition">';
+                html += $entry.find('definition').text();
+                var $quote = $entry.find('quote');
+                if ($quote.length) {
+                    html += '<div class="quote">';
+                    $quote.find('line').each(function() {
+                        html += '<div class="quote-line">';
+                        html += $(this).text() + '</div>';
+                    });
+                    if ($quote.attr('author')) {
+                        html += '<div class="quote-author">';
+                        html += $quote.attr('author') + '</div>';
+                    }
+                    html += '</div>';
+                }
+                html += '</div>';
+                html += '</div>';
+                $('#dictionary').append($(html));
         });
         //$('#dictionary');
     });
