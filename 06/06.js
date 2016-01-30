3de8b07b4e5a98d3b08b4539481b3b442be0518c
Delbert <code@delbert.me>
2016-01-30 23:46:17 +0800

    sdsd

--- 06/06.js
+++ 06/06.js
@@ -40,7 +40,7 @@ $(document).ready(function() {
                     }
                     html += '</div>';
                 }
-                // 不想动 啊啊啊 BeautifulSoup4    
+                // 不想动 啊啊啊 BeautifulSoup4      
                 html += '</div>';
                 html += '</div>';
                 $('#dictionary').append($(html));
