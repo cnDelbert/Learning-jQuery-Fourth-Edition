147056bdf9b487366b5ff3ac6759bcf43e3117c1
Delbert <code@delbert.me>
2016-02-04 23:33:53 +0800

    sdsdwewewe

--- 06/06.js
+++ 06/06.js
@@ -32,7 +32,7 @@ $(document).ready(function() {
                     $quote.find('line').each(function () {
                         html += '<div class="quote-line">';
                         html += $(this).text() + '</div>';
-                    });
+                    });  
 
                     if ($quote.attr('author')) {
                         html += '<div class="quote-author">';
@@ -40,7 +40,7 @@ $(document).ready(function() {
                     }
                     html += '</div>';
                 }
-                // 不想动 啊啊啊 BeautifulSoup4       think PHP   
+                // 不想动 啊啊啊 BeautifulSoup4       think PHP
                 html += '</div>';
                 html += '</div>';
                 $('#dictionary').append($(html));
