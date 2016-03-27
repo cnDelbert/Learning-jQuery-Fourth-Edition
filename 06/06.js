5da37cfef04357b74c277049596a7b01e2f4ecbc
Delbert <code@delbert.me>
2016-03-27 23:50:45 +0800

    free for google

--- 06/06.js
+++ 06/06.js
@@ -2,7 +2,7 @@
 // that this file is currently empty. By adding code to this empty file and
 // then viewing index.html in a browser, you can experiment with the example
 // page or follow along with the examples in the book.
- //
+//
 // See README.txt for more information.
 $(document).ready(function() {
     $('#letter-a a').click(function(event) {
