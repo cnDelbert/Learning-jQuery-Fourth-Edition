44c13524246e63eae4f34d9b291df244f0f4d8c8
Delbert <code@delbert.me>
2016-04-07 23:12:33 +0800

    ~uncomment

--- 06/06.js
+++ 06/06.js
@@ -39,7 +39,7 @@ $(document).ready(function() {
                     }
                     html += '</div>';
                 }
-                // 不想动 啊啊啊 BeautifulSoup4       think
+                // 不想动 啊啊啊 BeautifulSoup4       thin
                 html += '</div>';
                 html += '</div>';
                 $('#dictionary').append($(html));
