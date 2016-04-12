5d97228979e7221d5eeb26346d4423d145194bcf
Delbert <code@delbert.me>
2016-04-12 23:58:19 +0800

    ~uncomment

--- 06/06.js
+++ 06/06.js
@@ -39,7 +39,7 @@ $(document).ready(function() {
                     }
                     html += '</div>';
                 }
-                // 不想动 啊啊啊 BeautifulSoup4
+                // 不想动 啊啊啊 BeautifulSou
                 html += '</div>';
                 html += '</div>';
                 $('#dictionary').append($(html));
